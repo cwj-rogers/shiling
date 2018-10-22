5355cee1bed611fcb5d0f9d789c8676a82bc236b
roger <cwjackk@163.com>
2018-10-22 17:36:15 +0800

    新官网百度认证

--- hjzhome/views/layouts/hjz/side-head.php
+++ hjzhome/views/layouts/hjz/side-head.php
@@ -11,5 +11,6 @@
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="description" content="荟家装-让健康时尚的标准化家装走进千家万户，让家装交易更透明，更公平，更省钱。荟家装是一家致力于整体家装设计研发及交易管理服务平台，通过设计力量及供应链集采管理，让整体家装产品作品化加互联网全渠道营销为具备落地能力的小微装企及优秀项目经理，设计师输送业务订单。" />
 <meta name="keywords" content="荟家装|健康智慧整装|免费设计" />
+<meta name="baidu-site-verification" content="hfzddniCv7" />
 <link href="http://hjzhome.image.alimmdn.com/hjzWebsite/首页图/红底LOGO+500px.png" rel="shortcut icon" type="image/x-icon" />
 <link rel='stylesheet' href='/static/demo/metinfos.css'>
